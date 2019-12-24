2f741cc586cba7ff7ba15f4e7fe62be5f47e1d35
donglikang@pinzhikeji.com <kang1203>
2019-12-24 11:30:34 +0800

    login user validate

--- src/main/java/com/nuoke/sale/controller/LoginController.java
+++ src/main/java/com/nuoke/sale/controller/LoginController.java
@@ -1,12 +1,15 @@
 package com.nuoke.sale.controller;
 
 import com.google.common.collect.Maps;
+import com.google.gson.JsonObject;
 import com.nuoke.sale.util.RestResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Author:dlkang
@@ -17,9 +20,13 @@ public class LoginController {
 
     @RequestMapping("login/main")
     @ResponseBody
-    public RestResponse login() {
+    public RestResponse login(String userName, String password) {
 
-        Map<String,Object> map = Maps.newHashMap();
+        if (!Objects.equals(userName, "nuoke") || !Objects.equals(password, "nuoke123")) {
+            return RestResponse.failure("用户名或密码错误");
+        }
+
+        Map<String, Object> map = Maps.newHashMap();
         map.put("url", "index");
         return RestResponse.success("登录成功").setData(map);
     }
